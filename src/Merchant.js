844b4b782cebaf1a906e6e7504260ff6b63834a9
Dionysox <romainbasset1@gmail.com>
2018-09-18 18:59:48 +0200

    backgroundEnchanteur

--- src/Merchant.js
+++ src/Merchant.js
@@ -7,10 +7,14 @@ const styledItem = {
     cursor:"pointer",
 };
 const styledIcon = {
+	position: "relative",
     width: "30px",
     height: "30px",
-    float: "left",
+	float: "left",
+	marginTop: "3px",
+	marginLeft: "3px",
     display: "inline-block",
+	zIndex: "1",
 };
 const styledText = {
     marginLeft: "5px",
@@ -18,11 +22,20 @@ const styledText = {
     display: "inline-block",
 };
 
+const StyleBgIcon = {
+	position: "absolute",
+	width: "36px",
+    height: "36px",
+    float: "left",
+    display: "inline-block",
+}
+
 class Merchant extends Component {
     render() {
         const { name, items, icon, description, showItems } = this.props;
         return (
             <div style={styledItem} onClick={() => showItems(items)}>
+				<img src={"./equipement.png"}  style={StyleBgIcon}/>
                 <img
                     src={"./" + icon + ".jpg"}
                     alt={description}
